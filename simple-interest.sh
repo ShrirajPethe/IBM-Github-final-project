1d8d1a615d0eb008f6e3ee7ff132fd252e13d1ac
Shriraj Pethe <36429704+ShrirajPethe@users.noreply.github.com>
2025-02-07 09:05:14 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Original Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # ShrirajPethe
 
    # Input:
    # p, principal amount
